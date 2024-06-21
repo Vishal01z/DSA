d91aa2f90110df4dd53d6c1ef365383311efa821
Vishal Suryavanshi <107520324+Vishal01z@users.noreply.github.com>
2024-06-21 09:12:10 +0530

    Update Lecture 8 - Arrays Pt 2.cpp

--- Lecture 8 - Arrays Pt 2.cpp
+++ Lecture 8 - Arrays Pt 2.cpp
@@ -1,47 +1,21 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-//	int a,b,c,d,e;
-//	
-//	cout << "Give 5 integers as input"<<endl;
-//	cin >> a>> b>> c>> d>> e;
-//	
-//	cout << "The integers in reverse order are : "<<e<<d<<c<<b<<a;
-//	
 
-//    int a[4];
-//    int i;
-//    
-//    cout << "Give 5 integers as input"<<endl;
-//    
-//    for(i=0; i<5; i++)
-//    {
-//    	cin >> a[i];
-//	}
-//	cout << "The 5 given inputs are : "<<endl;
-//	
-//	for(i=0;i<5;i++)
-//	{
-//		cout<<a[i]<<" ";
-//	}
-
- // REVERSE ORDER
- 
- int a[4];
+int main() {
+    int a[10];  // Array size should be 10 to hold 10 integers
     int i;
     
-    cout << "Give 10 integers as input"<<endl;
+    cout << "Give 10 integers as input" << endl;
     
-    for(i=0; i<10; i++)
-    {
-    	cin >> a[i];
-	}
-	cout << "The 10 given inputs are : "<<endl;
-	
-	for(i=9;i>0;i--)
-	{
-		cout<<a[i]<<" ";
-	}
-	return 0;
+    for (i = 0; i < 10; i++) {
+        cin >> a[i];
+    }
+    
+    cout << "The 10 given inputs in reverse order are: " << endl;
+    
+    for (i = 9; i >= 0; i--) {
+        cout << a[i] << " ";
+    }
+    
+    return 0;
 }
